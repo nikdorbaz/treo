5db6c6c0ad83e7a0faf84ae814c79c5047c36a77
Vladimir Zabrodkin <36480753+nikdorbaz@users.noreply.github.com>
2019-11-22 14:45:01 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,42 @@
-window.onload = function(){
-    let menu = document.getElementById('nav-menu');
+window.onload = function() {
+    let menu = document.getElementById('nav-menu'),
+        login = document.getElementById('login'),
+        modal = document.getElementById('login-modal'),
+        anchorlinks = document.querySelectorAll('a[href^="#"]'),
+        body = document.body;
 
-    menu.onclick = function(){
+    menu.onclick = function(e) {
         this.classList.toggle('open');
     }
+
+    login.onclick = function(e) {
+        e.preventDefault();
+        body.classList.add('modal-active');
+    }
+
+    document.addEventListener('click', function(e) {
+        let isModal = modal.contains(e.target);
+
+        if (!isModal && e.target != login) {
+            body.classList.remove('modal-active');
+        }
+    });
+
+    anchorlinks.forEach(function(el) {
+        el.addEventListener('click', function(e) {
+            e.preventDefault();
+            let target = document.getElementById(this.hash.replace('#', ''));
+
+            if (!target) { return }
+
+            if (window.clipboardData || navigator.userAgent.search(/YaBrowser/) >= 0) {
+                window.scrollTo(0, target.offsetTop);
+            } else {
+                window.scrollTo({
+                    top: target.offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
 }
